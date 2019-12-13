2a8ee2de22b06c583de156b1c5e7280c4b71d9f8
Dominika Jadowska <domi@imagine.team>
2019-12-13 02:51:19 +0100

    Most GUI Complete

--- app.js
+++ app.js
@@ -1,14 +1,32 @@
-const electron = require('electron');
 const url = require('url');
 const path = require('path');
+const glob = require('glob');
+const {
+    app,
+    BrowserWindow
+} = require('electron');
 
-const {app, BrowserWindow} = electron;
+if (process.mas) app.setName('ImagineCraft');
 
-let mainWindow;
+let mainWindow = null;
+
+app.on('window-all-closed', function () {
+    app.quit();
+});
 
 //Listen for app to ready
-app.on('ready', function(){
-    mainWindow = new BrowserWindow({});
+app.on('ready', function () {
+    mainWindow = new BrowserWindow({
+        resizable: false,
+        width: 1100,
+        height: 600,
+        transparent: true,
+        frame: false,
+        webPreferences: {
+            webviewTag: true,
+            nodeIntegration: true
+        }
+    });
     //Load HTML into window
     mainWindow.loadURL(url.format({
         pathname: path.join(__dirname, 'news.html'),
